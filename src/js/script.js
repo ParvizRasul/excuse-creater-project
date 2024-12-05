353e5f274bbc3ad3d46a0e4f29b4466e03ae2c2c
parvizrasulov <resulovperviz94@gmail.com>
2024-12-04 16:06:07 -0800

    execute creater

--- src/js/script.js
+++ src/js/script.js
@@ -5,10 +5,28 @@ window.onload = function () {
 
     // 1. Get the excuse element
     const excuse = document.getElementById('excuse');
+    console.log(excuse);
+
+    const part1 = ["my fake eyelashes","my grandmother's body", "my cat", "I", " someone " ];
+    const part2 = ["got", "was exhumed", "found out", "got bit", "glued"];
+    const part3 = ["stuck ", "for", "where", "by", "my doors and windows"];
+    const part4 = ["together", "a police investigation", "I'm ticklish", "a duck", "shut"];
+
 
     // 2. Generate a random excuse
 
-    const excuseText = `The dog ate my homework for breakfast`;
+    // const indexPart1 = [Math.floor(Math.random()*part1.length)];
+    // const indexPart2 = Math.floor(Math.random()*part2.length);
+    // const indexPart3 = Math.floor(Math.random()*part3.length);
+    // const indexPart4 = Math.floor(Math.random()*part4.length);
+
+    const index = Math.floor(Math.random()*part1.length);
+  
+
+    const excuseText = `${part1[index]} ${part2[index]} ${part3[index]} ${part4[index]}`;
+    console.log(excuseText);
+    
+   
 
     // 3. Inseart the excuse into the excuse element
     excuse.innerHTML = excuseText;
